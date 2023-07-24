542bccea322282601eb7a4b21c7e2b252c9f4c4b
jeongbin5211 <hjn123493@gmail.com>
2023-07-24 16:12:04 +0900

    0724

---
+++ typescript/src/0630/a_generic.js
@@ -0,0 +1,85 @@
+// 제네릭(Generic)
+// 다양한 타입을 하나의 표현식으로 처리할 수 있는 프로그래밍 기법 중 하나
+// 코드의 중복을 줄이고, 재사용성을 높이며, 타입 안정성을 보장할 수 있는 기능
+// 재사용성이 높은 컴포넌트를 만들 때 자주 활용 (컴포넌트는 한가지 타입보다 여러 가지 타입에서 동작)
+//! 제네릭 사용 법 & 호출
+// 타입을 마치 함수의 매개변수(파라미터)처럼 사용하는 것을 의미
+function getText(text) {
+    return text;
+}
+getText("hi"); // 'hi'
+getText(10); // 10
+getText(true); // true
+function getTextGeneric(text) {
+    return text;
+}
+getTextGeneric("hi"); // 'hi'
+getTextGeneric(20); // 20
+getTextGeneric(false); // false
+function logText(text) {
+    return text;
+}
+// 1.
+var text = logText("Hello Generic");
+// 2.
+var text2 = logText("Hello Generic");
+//! 제네릭 타입 변수
+function logTextError(text) {
+    // console.log(text.length); => 'T' 형식에 'length' 속성이 없습니다.
+    return text;
+}
+//! 배열 제네릭 표시 (Array<T>, T[])
+// 함수 코드는 T라는 변수 타입을 받고, 매개변수 값으로는 배열 형태인 T를 받습니다.
+// 매개변수: [1, 2, 3], 반환 값: 3(number)
+function logTextLength(text) {
+    console.log(text.length);
+    return text;
+}
+function logTextLength2(text) {
+    console.log(text.length);
+    return text;
+}
+//! 제네릭 타입
+//? 제네릭 인터페이스
+function logTextType(text) {
+    return text;
+}
+// 1.
+var str = logTextType;
+// 2.
+var str2 = logTextType;
+function logTextGenericType(text) {
+    return text;
+}
+var myString = logTextGenericType;
+function logTextGenericType2(text) {
+    return text;
+}
+var myString2 = logTextGenericType2;
+//! 제네릭 클래스
+var Box = /** @class */ (function () {
+    function Box(item) {
+        this.item = item;
+    }
+    Box.prototype.getItem = function () {
+        return this.item;
+    };
+    return Box;
+}());
+var box1 = new Box(42);
+console.log(box1.getItem); // 42
+var box2 = new Box("hello");
+console.log(box2.getItem); // "hello"
+function logTextError2(text) {
+    console.log(text.length);
+    return text;
+}
+// logTextError2(10) 숫자타입에는 length속성이 존재하지 않으므로 오류
+logTextError2({ length: 0, value: "hi" }); // 0
+// text.length 코드는 객체의 속성 접근과 같이 동작하므로 오류가 나지 않음
+//! 객체의 속성을 제약하는 방법
+function getProperty(obj, key) {
+    return obj[key];
+}
+var objects = { a: 1, b: 2, c: 3 };
+getProperty(objects, 'a');
